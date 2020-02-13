25f3b8f570b456bb649c6548fdc10e6229f1753f
BillNyeTheGamerGuy <howard327@morrisville.edu>
2020-02-13 14:43:48 -0500

    Create(d) a public method.

--- Assets/Scripts/AdventureGame.cs
+++ Assets/Scripts/AdventureGame.cs
@@ -7,11 +7,15 @@ public class AdventureGame : MonoBehaviour
 {
 
     [SerializeField] Text txtComponent;
+    [SerializeField] State startingState;
+
+    State state;
 
     // Start is called before the first frame update
     void Start()
     {
-        txtComponent.text = ("Another busy day at work.");
+        state = startingState;
+        txtComponent.text = state.GetStateStory();
     }
 
     // Update is called once per frame
--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject
 {
-
     [TextArea(10,14)] [SerializeField] string strStoryText;
+
+    public string GetStateStory()
+    {
+        return strStoryText;
+    }
 
 }
